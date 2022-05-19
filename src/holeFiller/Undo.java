853c3c0c49783e714a48074b4c13e4101f5d5e09
kaplego <70806521+kaplego@users.noreply.github.com>
2022-05-19 20:49:52 +0200

    Send a message if there is no last hole

--- src/holeFiller/Undo.java
+++ src/holeFiller/Undo.java
@@ -1,10 +1,12 @@
 package holeFiller;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
+import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,13 +26,23 @@ public class Undo extends Command implements TabExecutor {
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if (!(commandSender instanceof Player)) return false;
+
+        if (locs.size() != typs.size()) this.clear();
+
+        if (locs.size() == 0 || typs.size() == 0) {
+            commandSender.sendMessage(ChatColor.RED+"Cannot find the last filled hole.");
+            return true;
+        }
+
         for (int i = 0; i < locs.size(); i++) {
             Location loc = locs.get(i);
             Material typ = typs.get(i);
 
             loc.getBlock().setType(typ);
         }
-        return false;
+        this.clear();
+        return true;
     }
 
     @Override
